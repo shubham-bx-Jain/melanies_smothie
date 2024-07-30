80fdfa353daf53a0b42b9f7e356b06fac56759f7
shubham-bx-Jain <shubham495.jain@gmail.com>
2024-07-30 19:36:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ if ingredients_list:
       session.sql(my_insert_stmt).collect()
       st.success('Your Smoothie is ordered, ' + Name_on_Order + '!', icon="✅")
     import requests
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-     #st.text(fruityvice_response.json())
-        fv_df = st.dataframe(fruityvice_response.json() , use_container_width=True)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    #st.text(fruityvice_response.json())
+    fv_df = st.dataframe(fruityvice_response.json() , use_container_width=True)
     
