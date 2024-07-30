e4e59715657610f417f617989c89242b59a6adab
shubham-bx-Jain <shubham495.jain@gmail.com>
2024-07-30 11:36:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,4 +35,4 @@ if time_to_insert:
    st.success('Your Smoothie is ordered, ' + Name_on_Order + '!', icon="✅")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
